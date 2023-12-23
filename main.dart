e1559c8a3faaeea93c43abfc8f1d12bce5fa7e05
Aditiv12 <100767751+Aditiv12@users.noreply.github.com>
2023-12-23 22:11:42 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(1, 27, 105, 184));
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 20,
+              ),
+            ),
+      ),
+      home: const Expenses(),
+    ),
+  );
+}
